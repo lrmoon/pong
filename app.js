ba6849e635fa8d56eef6ff97a45f48ca290996e0
lrmoon <larariad@rocketmail.com>
2021-09-27 20:01:19 -0400

    send stuff

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const player1 = {
     yPos: 390,
     height: 35,
     width: 130,
-    score: 0
+    score: 0,
+    velocity: 10
 
 }
 
@@ -22,7 +23,8 @@ const player2 = {
     yPos: 390,
     height: 35,
     width: 130,
-    score: 0
+    score: 0,
+    velocity: 10
 
 }
 
@@ -55,17 +57,10 @@ function initialize(){
     player2.yPos = 390;
 
     
-    render();
+    
 }
 
-function ballF(x, y){
-    this.x = ball.xPos;
-    this.y = ball.yPos;
 
-    this.draw = function(){
-        console.log('asdha')
-    }
-}
 
 // var ball = new ballF(xCenter, yCenter);
 // ballF.draw();
@@ -80,58 +75,123 @@ function render(){
     c.fillText('pong', xCenter, 30);
 }
 
+
+
+// const player1input = () => {
+   
+//     if(38 in keys){
+//         console.log('hi');
+//         player1.yPos -= player1.velocity;
+//     }
+//     if(40 in keys){
+//         player1.yPos += player1.velocity;
+//     }
+    
+// }
 function movePaddle(e){
     switch(e.keyCode) {
         case 38: //arrow up
-            player2.posY -= 40; //move up
+           
+            player2.posY -= 40;
+            console.log('stuffup');
+             //move up
             break;
         case 40: //arrow down
-            player2.posY += 40; //move down
+            player2.posY += 40;
+            console.log('stuffdown');//move down
             break;
         case 87: //w key
-            player1.posY -= 40; //move up
+            player1.posY -= 40;
+            console.log('stuffupw');//move up
             break;
-        case 40: // s key
-            player1.posY += 40; //move down
+        case 83: // s key
+            player1.posY += 40;
+            console.log('stuffdowns')//move down
             break;
     }
     e.preventDefault();
-    render();
+    
+    
+
+    
 }
 
-function animate(){
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0 , innerWidth, innerHeight);
+// function animate(){
+//     requestAnimationFrame(animate);
+//     c.clearRect(0, 0 , innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(ball.xPos, ball.yPos,30, 0, Math.PI * 2,false);
-    c.fillStyle = 'black';
-    c.stroke();
+//     c.beginPath();
+//     c.arc(ball.xPos, ball.yPos,30, 0, Math.PI * 2,false);
+//     c.fillStyle = 'black';
+//     c.stroke();
 
    
-    if(ball.xPos + ball.radius > innerWidth || ball.xPos - ball.radius < 0){
-        ball.dx = -ball.dx;
+//     if(ball.xPos + ball.radius > innerWidth || ball.xPos - ball.radius < 0){
+//         ball.dx = -ball.dx;
+//     }
+    
+//     if(ball.yPos + ball.radius > innerHeight  || ball.yPos - ball.radius < 0){
+//         ball.dy = -ball.dy;
+//     }
+
+//     ball.xPos += ball.dx;
+//     ball.yPos += ball.dy;
+
+//     player1input();
+//     render();
+
+// }
+
+// animate();
+
+function DrawBall(x,y,dx,dy){
+this.x = ball.xPos;
+this.y = ball.yPos;
+this.dx = dx;
+this.dy = dy;
+
+this.draw = function(){
+    c.beginPath();
+    c.arc(this.x, this.y,30, 0, Math.PI * 2,false);
+    c.fillStyle = 'black';
+    c.stroke();
+}
+
+this.move = function(){
+    if(this.x + ball.radius > innerWidth || this.x - ball.radius < 0){
+        this.dx = -this.dx;
     }
     
-    if(ball.yPos + ball.radius > innerHeight  || ball.yPos - ball.radius < 0){
-        ball.dy = -ball.dy;
+    if(this.y + ball.radius > innerHeight  || this.y - ball.radius < 0){
+        this.dy = -this.dy;
     }
 
-    ball.xPos += ball.dx;
-    ball.yPos += ball.dy;
+    this.x += this.dx;
+    this.y += this.dy;
 
+    player1input();
     render();
-
 }
 
-animate();
-
-function drawBall(){
 
 }
+var ball2 = new DrawBall(200,200);
 
 function drawRectagle(){
     
 }
+function ballCollision(){
+   
+}
+
 document.addEventListener('keydown', movePaddle);
+
+document.addEventListener('keydown', function(e){
+    keys[e.keycode] = true;
+    
+    e.preventDefault;
+})
+document.addEventListener('keyup', function(e){
+    delete keys[e.keycode];
+})
 
