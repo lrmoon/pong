bee022f9ecb75b568ac5a8ae3c8c978db141b041
lrmoon <larariad@rocketmail.com>
2021-09-29 16:19:57 -0400

    fixed collison detector, added score update,rendered winning message

--- app.js
+++ app.js
@@ -3,28 +3,28 @@
 let canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 //draw items on screen
-const xCenter = canvas.width/2;
-const yCenter = canvas.height/2;
+let winner = false;
+let headerText = `Now playing...`
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const player1 = {
-    xPos: 50,
+    xPos: innerWidth/50,
     yPos: 390,
     height: 35,
     width: 130,
-    score: 0,
-    velocity: 10
+    score: 0
+    
 
 }
 
 const player2 = {
-    xPos: 1470,
+    xPos: innerWidth - 70,
     yPos: 390,
     height: 35,
     width: 130,
-    score: 0,
-    velocity: 10
+    score: 0
+    
 
 }
 
@@ -40,21 +40,29 @@ const ball = {
 
 
 
-
-
-
-
-
-
-
-
-
 //drawCircle(xCenter,yCenter,30,'black');
 
 initialize();
 function initialize(){
+    player1.xPos = innerWidth/50;
     player1.yPos = 390;
+    player1.height = 35;
+    player1.width = 130;
+    
+    
+    player2.xPos = innerWidth - 70;
     player2.yPos = 390;
+    player2.height =  35;
+    player2.width = 130;
+    
+
+    ball.xPos = innerWidth/2;
+    ball.yPos = innerHeight/2;
+    ball.radius = 30;
+    ball.color = 'black';
+    ball.dx = (Math.random() - 0.5 )* 15; //xvelocity
+    ball.dy = (Math.random() - 0.5 )* 15; //yVelocity
+    ball.radius = 30;
 
     
     
@@ -62,8 +70,7 @@ function initialize(){
 
 
 
-// var ball = new ballF(xCenter, yCenter);
-// ballF.draw();
+
 function render(){
     
     c.fillRect(player1.xPos,player1.yPos,player1.height,player1.width);
@@ -74,29 +81,21 @@ function render(){
     c.font = '30px Arial';
     c.fillText(`Score: ${player1.score}`  , 50, 30);
     c.fillText(`Score: ${player2.score}`, 1390, 30);
+    c.fillText(headerText, innerWidth/4 -50, 100);
 }
 
 
 
-// const player1input = () => {
-   
-//     if(38 in keys){
-//         console.log('hi');
-//         player1.yPos -= player1.velocity;
-//     }
-//     if(40 in keys){
-//         player1.yPos += player1.velocity;
-//     }
-    
-// }
+
 function movePaddle(e){
     switch(e.keyCode) {
         case 38: //arrow up
            
-            player2.yPos -= 40;
+            
             if(player1.yPos === innerHeight){
                 break;
             }
+            player2.yPos -= 40;
              //move up
             break;
         case 40: //arrow down
@@ -114,14 +113,13 @@ function movePaddle(e){
     }
     e.preventDefault();
     
-    
-
-    
 }
 
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0, 0 , innerWidth, innerHeight);
+    
+    render();
 
     c.beginPath();
     c.arc(ball.xPos, ball.yPos,30, 0, Math.PI * 2,false);
@@ -129,22 +127,38 @@ function animate(){
     c.fill();
     c.stroke();
 
-   
-    if(ball.xPos + ball.radius > innerWidth || ball.xPos - ball.radius < 0){
-        ball.dx = -ball.dx;
-    }
-    
-    if(ball.yPos + ball.radius > innerHeight  || ball.yPos - ball.radius < 0){
-        ball.dy = -ball.dy;
-    }
+    if(winner === false){
+        if(ball.xPos + ball.radius > innerWidth){ //right side (player 2)
+            player1.score += 1;
+            player1.score === 3 ? winner = true : winner = false;
+            initialize();
+            
+        }else if (ball.xPos - ball.radius < 0) { //left side (player 1)
+            player2.score+= 1;
+            player1.score === 3 ? winner = true : winner = false;
+            initialize();
+        }
+        
+        if(ball.yPos + ball.radius > innerHeight  || ball.yPos - ball.radius < 0){
+            ball.dy = -ball.dy;
+        }
 
-    ballCollision();
-    
-    ball.xPos += ball.dx;
-    ball.yPos += ball.dy;
-
+        ballCollision();
+        
+        ball.xPos += ball.dx;
+        ball.yPos += ball.dy;
+  }else if (winner === true){
+      if(player1.score === 3){
+        headerText = 'Congrats Player 1! You have won the game of pong!!'
+      }else if(player2.score === 3){
+        headerText = 'Congrats Player 2! You have won the game of pong!!';
+      
+      }
+      
+      
+  }
     //player1input();
-    render();
+    
 
 }
 
@@ -177,11 +191,21 @@ function ballCollision(){
    let ballRight = ball.xPos + ball.radius;
 
    //if there is a collison on any side of the paddle with the ball
-   
-    if(ballLeft < rightOfPaddle1 || ballRight > leftOfPaddle2 ){
-        ball.dx = -ball.dx;
-   }
+    if((ballLeft < rightOfPaddle1 && ballRight > leftOfPaddle1 && ballBottom >topOfPaddle1 && ballTop < bottomOfPaddle1) || (ballLeft < rightOfPaddle2 && ballRight > leftOfPaddle2 && ballBottom >topOfPaddle2 && ballTop < bottomOfPaddle2)){
+        ball.dx = -ball.dx
+
+    }
 }
+
+
+
 
 document.addEventListener('keydown', movePaddle);
 
+// window.addEventListener('resize',resizeCanvas);
+
+// function resizeCanvas(){
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
+//     animate();
+// }
